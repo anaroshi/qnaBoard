2faafd5e5dd669d90b3807404f29f49a252df13c
anaroshi <sundor@hanmail.net>
2023-09-23 08:15:08 +0900

    setting

---
+++ src/main/java/com/sundor/qnaBoard/QnaBoardApplication.java
@@ -0,0 +1,14 @@
+package com.sundor.qnaBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnaBoardApplication {
+
+	public static void main(String[] args) {
+		System.out.println(".... success....");
+		SpringApplication.run(QnaBoardApplication.class, args);
+	}
+
+}
